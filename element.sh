48420c110d84ba1cd6cfd4197fa801b9b73f5267
Apinda Tekula <nerdcod3rx@gmail.com>
2025-03-19 07:50:28 +0000

    feat:handling-atomic-numbers-input

--- element.sh
+++ element.sh
@@ -11,7 +11,27 @@ arg=$1
 # if atomic number is provided
 if [[ $arg =~ ^[0-9]+$ && $arg -gt 0  ]]
 then
-    echo -e "\nYou entered an atomic number."
+    # retrieve element information
+    ELEMENT=$($PSQL "SELECT * FROM elements WHERE atomic_number=$arg")
+    # if element is not found
+    if [[ -z $ELEMENT ]]
+    then
+        echo "Element is not found."
+    else
+        echo "$ELEMENT" | while IFS="|" read ATOMIC_NUMBER SYMBOL NAME
+        do
+           # get the properties info of the element. 
+           ELEMENT_PROPERTIES=$($PSQL "SELECT * FROM properties WHERE atomic_number=$arg")
+           # parse the information and print the statement to the user.
+           echo "$ELEMENT_PROPERTIES" | while IFS="|" read ATOMIC_NUMBER_PROPERTIES TYPE ATOMIC_MASS MELTING_POINT_CELSIUS BOILING_POINT_CELSIUS TYPE_ID
+           do
+                # get the type_name of the metal from types table
+                TYPE_NAME=$($PSQL "SELECT type FROM types WHERE type_id=$TYPE_ID" )
+                # echo the output
+                echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME. It's a $TYPE_NAME, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+           done
+        done
+    fi
 
 # if element symbol is provided (max of 2 characters)
 elif [[ $arg =~ ^[A-Z][a-z]{0,1}$ ]]
