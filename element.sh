70ad11622024e098df8ba5d51073b224dab79d31
Apinda Tekula <nerdcod3rx@gmail.com>
2025-03-19 07:25:54 +0000

    feat: input-validation-conditions-added

--- element.sh
+++ element.sh
@@ -9,5 +9,16 @@ fi
 # argument provided
 arg=$1
 # if atomic number is provided
+if [[ $arg =~ ^[0-9]+$ && $arg -gt 0  ]]
+then
+    echo -e "\nYou entered an atomic number."
+
 # if element symbol is provided (max of 2 characters)
+elif [[ $arg =~ ^[A-Z][a-z]{0,1}$ ]]
+then
+    echo -e "\nYou entered the symbol of the element."
 # if element name is provided
+elif [[ $arg =~ ^[A-Za-z]+$ ]]
+then
+    echo -e "\nYou entered the element name."
+fi
